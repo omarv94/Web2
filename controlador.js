419790e05705157177e5cfdc22ed38ec06f31d06
Omar Valencia <omarvalencia9415@gmail.com>
2020-10-08 18:42:18 -0500

    new controler

--- controlador.js
+++ controlador.js
@@ -1,27 +1,43 @@
- const titulo   =document.getElementById('titulo'),
-       anterior =document.getElementById('anterior'),
-       siguiente=document.getElementById('siguiente'); 
+const llave1 = "grant_type=client_credentials",
+  llave2 = "client_id=71685fc3bc13460d8ed0649c968a5925",
+  llave3 = "client_secret=30bde0b442f54259bd299beb6ac3b1ad";
 
-
- anterior.addEventListener('click',pistaAnterior);
-
- const pistaAnterior=()=>{
-     
- }
- 
- 
- titulo.textContent='coldplay';
- 
- 
- const url ="https://api.spotify.com/v1/artists/3JCyEziWwDZksR1WY8EckM/top-tracks?country=US&=",
-   toquen ="Bearer BQAHTj6ws--bz87reWb35tv90BAwxhHHfM5qaV9-5hbpKNhf1QjeJS9w5kqXTajy3YIALi0pEWaKCFo3cHodgAWKCmdXXwC_IKENONZITXcmDynEjcAFKP8mkTXwTC0n9ts_imE5_3q3VDa7k9KG89Pzq5rWha4";
-
-const parametros ={
-    method :'GET',
-    headers :{
-        'Authorization' :toquen
-    }
+const parametrosPOST = {
+  method: "POST",
+  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  body: `${llave1}&${llave2}&${llave3}`,
 };
-fetch(url,parametros) 
+
+const urlPOST = " https://accounts.spotify.com/api/token";
+
+fetch(urlPOST, parametrosPOST)
+  .then((respuesta) => respuesta.json())
+  .then((datos) => obtenerToken(datos));
+
+const obtenerToken = (datos) => {
+  let token = datos.access_token;
+  token=`Bearer ${token}`;
+  const parametrosGET = {
+    method: "GET",
+    headers: { Authorization: token },
+  };
+  const urlGET =
+    "https://api.spotify.com/v1/artists/1GImnM7WYVp95431ypofy9/top-tracks?market=ES";
+
+  fetch(urlGET, parametrosGET)
     .then(respuesta => respuesta.json())
-    .then(datos =>console.log(datos))
+    .then(datos => depurarArtista(datos));
+};
+
+const depurarArtista=(datos)=>{
+    let nombre1=(datos.tracks[0].name);
+    let audio1=(datos.tracks[0].preview_url);
+    let imagen1=(datos.tracks[0].album.images[0].url);
+
+
+    for (i=0; i <= datos.lengt; i ++)
+    {
+        datos[i].name
+    }
+
+}
